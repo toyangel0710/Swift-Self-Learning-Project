4829edf872cdbcb3720865ff45d58ec9d598b667
James <toyangel0710@gmail.com>
2018-02-05 23:37:09 +0800

    提交homework:PaperScissorsStone程式碼

--- 0. HomeWork/PaperScissorsStone/PaperScissorsStone/ViewController.swift
+++ 0. HomeWork/PaperScissorsStone/PaperScissorsStone/ViewController.swift
@@ -11,6 +11,52 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var buttonCollection: [UIButton]!
+    @IBOutlet weak var winLabel: UILabel!
+    @IBOutlet weak var loseLabel: UILabel!
+    @IBOutlet weak var yourPlayImage: UIImageView!
+    @IBOutlet weak var systemPlayImage: UIImageView!
+    
+    var winCount = 0 {
+        didSet {
+            winLabel.text = "你贏了\(winCount)次"
+        }
+    }
+    
+    var loseCount = 0 {
+        didSet {
+            loseLabel.text = "你輸了\(loseCount)次"
+        }
+    }
+    var systemSign = -1 {
+        didSet {
+            switch systemSign {
+            case 0:
+                systemPlayImage.image = UIImage(named:"paper")
+            case 1:
+                systemPlayImage.image = UIImage(named:"scissors")
+            case 2:
+                systemPlayImage.image = UIImage(named:"stone")
+            default:
+                systemPlayImage.image = nil
+            }
+        }
+    }
+    
+    var yourSign = -1 {
+        didSet {
+            switch yourSign {
+            case 0:
+                yourPlayImage.image = UIImage(named:"paper")
+            case 1:
+                yourPlayImage.image = UIImage(named:"scissors")
+            case 2:
+                yourPlayImage.image = UIImage(named:"stone")
+            default:
+                yourPlayImage.image = nil
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,12 +64,33 @@ class ViewController: UIViewController {
             button.imageView?.contentMode = .scaleAspectFit
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func playGame(_ sender: UIButton) {
+        yourSign = buttonCollection.index(of: sender)!
+        systemSign = Int(arc4random_uniform(3))
+        
+        switch yourSign {
+        case 0:
+            switch systemSign {
+            case 1 : loseCount += 1
+            case 2 : winCount += 1
+            default: return
+            }
+        case 1:
+            switch systemSign {
+            case 0 : winCount += 1
+            case 2 : loseCount += 1
+            default: return
+            }
+        case 2:
+            switch systemSign {
+            case 0 : loseCount += 1
+            case 1 : winCount += 1
+            default: return
+            }
+        default:
+            return
+        }
     }
-
-
 }
 
