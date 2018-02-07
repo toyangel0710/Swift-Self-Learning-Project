c55d0b64febe699261b16e7f2a7ae91822ee105b
James <toyangel0710@gmail.com>
2018-02-07 12:25:31 +0800

    完成TableViewDemo實作

--- 4. TableView/TableViewDemo/TableViewDemo/FoodTableViewController.swift
+++ 4. TableView/TableViewDemo/TableViewDemo/FoodTableViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class FoodTableViewController: UITableViewController {
     var foodNames = ["漢堡","咖哩","調酒","蛋捲","炸雞","炒麵","炒飯","炸春捲","熱狗","冰淇淋","小羊排","月亮蝦餅","鬆餅","義大利麵","披薩","沙拉","生魚片","湯包","壽司","玉米餅"]
     var foodImages = ["buger","curry","drink","egg_roll","fried_chicken","fried_noodle","fried_rice","fried_spring_roll","hotdog","ice_cream","lamb","moon_shrimp_cake","pancake","pasta","pizza","salad","sashimi","soup_dumplings","suhi","taco"]
+    var foodLocation = ["台北","台中","台北","高雄","台北","高雄","高雄","台南","台北","台北","台北","高雄","台北","台中","台北","台北","高雄","高雄","台北","屏東",]
+    var foodType = ["美式","印度料理","飲料","小吃","小吃","小吃","小吃","點心","點心","甜點","西式","越式","點心","義式","義式","美式","日式","台式","日式","墨西哥料理"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -45,6 +48,8 @@ class FoodTableViewController: UITableViewController {
         cell.foodImage.image = UIImage(named:foodImages[indexPath.row])
         cell.foodImage.layer.cornerRadius = 30.0
         cell.foodImage.clipsToBounds = true
+        cell.foodLocation.text = foodLocation[indexPath.row]
+        cell.foodType.text = foodType[indexPath.row]
         return cell
     }
  
